16570d104a6f4d383c0367a9e35d197c2c2b0a6c
Biel Casamor <bielcasamor02@gmail.com>
2024-02-21 15:10:49 +0100

    inicio projecto dos

---
+++ projecto_dos/src/Fila.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+export const filas = [
+
+    {
+        producto:"manzana",
+        cantidad: 2,
+        precio: 3.4,
+        tienda: "Mercadona",
+        notas: "Comprar 3",
+        comprado: true,
+        imagen: "Pollo.jpg",
+    },
+    {
+        producto:"platano",
+        cantidad: 1,
+        precio: 0.33,
+        tienda: "platano",
+        notas: "platano",
+        comprado: true,
+        imagen: "platano_del_platano",
+    },
+    {
+        producto:"kiwi",
+        cantidad: 5,
+        precio: 17.99,
+        tienda: "new Zeland",
+        notas: "el animal no la fruta",
+        comprado: true,
+        imagen: "URL de una imagen de  un kiwi",
+
+    }
+];
