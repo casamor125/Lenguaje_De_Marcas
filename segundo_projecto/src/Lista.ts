b6e01ed9021286870e08d94642fd77866eba1b34
Biel Casamor <bielcasamor02@gmail.com>
2024-02-26 20:28:51 +0100

    react tabla acabada en teoria

---
+++ segundo_projecto/src/Lista.ts
@@ -0,0 +1,61 @@
+import React from "react";
+import pollo from "./Pollo.jpg"
+
+export const filas = [
+
+    {
+        producto:"manzana",
+        cantidad: 2,
+        precio: 3.4,
+        tienda: "Mercadona",
+        notas: "Comprar 3",
+        comprado: true,
+        imagen: pollo,
+    },
+    {
+        producto:"manzana",
+        cantidad: 2,
+        precio: 3.4,
+        tienda: "Mercadona",
+        notas: "Comprar 3",
+        comprado: true,
+        imagen: pollo,
+    },
+    {
+        producto:"manzana",
+        cantidad: 2,
+        precio: 3.4,
+        tienda: "Mercadona",
+        notas: "Comprar 3",
+        comprado: true,
+        imagen: pollo,
+    },
+    {
+        producto:"manzana",
+        cantidad: 2,
+        precio: 3.4,
+        tienda: "Mercadona",
+        notas: "Comprar 3",
+        comprado: true,
+        imagen: pollo,
+    },
+    {
+        producto:"platano",
+        cantidad: 1,
+        precio: 0.33,
+        tienda: "platano",
+        notas: "platano",
+        comprado: true,
+        imagen: pollo,
+    },
+    {
+        producto:"kiwi",
+        cantidad: 5,
+        precio: 17.99,
+        tienda: "new Zeland",
+        notas: "el animal ",
+        comprado: true,
+        imagen: pollo,
+
+    }
+];
