9f530cc7c3fc7042eeaf47ae436fcca6e69c858d
Biel Casamor <bielcasamor02@gmail.com>
2024-03-06 17:45:36 +0100

    guia turistica

--- guia_react/src/Inici.tsx
+++ guia_react/src/Inici.tsx
@@ -1,8 +1,23 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.css';
+import foto from "./p1.jpeg";
 
 function Inici(){
     return(
-        <p>Inici</p>
+        <div className ="flex-column" >
+            
+            <div className="d-flex flex-row align-self-center">
+                <div >
+                <h2>Guias Mallorca</h2>
+                <p>Nuestra guia Turística de malloraca proporciona Los mejores hoteles de la isla con las mejores actividades, así como los puntos de interes de la isla y los restaurantes crecanos</p>
+                </div> 
+                <div >
+                    <img src={foto} alt="" />
+                    
+                </div>
+            </div>
+            <div className="align-self-center"> <p>pepe</p></div>
+        </div>
 
     );
 }
